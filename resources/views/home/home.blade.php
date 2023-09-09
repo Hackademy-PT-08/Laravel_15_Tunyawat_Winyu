52c19cff646b5a9016cb7504ebac6ad513ca98ad
Tuntunyawat <twinyu0245@gmail.com>
2023-09-09 22:04:27 +0200

    Laravel_Livewire pulsanti piu e meno

---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,7 @@
+@extends('components.master')
+
+
+@section('content')
+    <h1 class="text-center">This is counter</h1>
+    @livewire('counter')
+@endsection
---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,5 @@
+<div class="text-center mt-5">
+    <button wire:click='increment'>+</button>
+    <span>{{ $counter }}</span>
+    <button wire:click='decrement'>-</button>
+</div>
